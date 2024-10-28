6f72ea21faaecccf8abf4ff44d432fd2d9de2443
Ankur Singh <70473170+ankursingh11@users.noreply.github.com>
2024-10-28 16:18:56 +0530

    Deleted Some files

--- Striver A2Z/Step 13 - Binary Trees [Traversals, Medium and Hard Problems]/Lec 1 - Traversals/introduction-to-trees.cpp
+++
@@ -1,9 +0,0 @@
-
-// https://www.geeksforgeeks.org/problems/introduction-to-trees/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=introduction-to-trees
-
-class Solution {
-  public:
-    int countNodes(int i) {
-        return pow(2, i-1);
-    }
-};
